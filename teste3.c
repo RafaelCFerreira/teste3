7d61b606f300159c66a8216c795ceb988f9c9bbd
RafaelCF <rafaelcferreira232@gmail.com>
2021-12-21 11:51:10 -0300

    add teste3.c teste3 e delete calculos.c

--- teste3.c
+++ teste3.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+float calcularF(float);
+float calcularK(float);
+
 int main()
 {
-    float c, f, k;
+    float c;
     printf("Entre com o valor de Celsius.\n");
     scanf("%f", &c);
+    printf("Valor da temp em Fahrenheit é %.2f e o valor da temp em Kelvin é %.2f\n", calcularF(c), calcularK(c));
     return 0;
+}
+
+float calcularF(float c)
+{
+        return (9 * c + 160)/5;
+}
+
+float calcularK(float c)
+{
+        return c + 273;
 }
 
